8d0247d06e695f738c4822f9f5a4fa4a2e750232
Олег <oleg.bignyac@gmail.com>
2022-10-23 12:35:56 +0300

    second homework

---
+++ data_types.py
@@ -0,0 +1,23 @@
+login = 'admin'
+name_dog = 'Smith'
+steam_nickname = 'Dune'
+money = 'Euro'
+child = 'Bob'
+password = 2334
+chat_id = 2222
+number_phone = 123456
+x = 34
+b = 22
+print(login, steam_nickname, money, child, password,
+      chat_id, number_phone, x, b)
+
+print(type(login))
+print(type(name_dog))
+print(type(steam_nickname))
+print(type(money))
+print(type(child))
+print(type(password))
+print(type(chat_id))
+print(type(number_phone))
+print(type(x))
+print(type(b))
